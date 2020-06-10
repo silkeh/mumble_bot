3fb2bd15280117eaa8bf93039528926871b276c7
Silke Hofstra <silke@slxh.eu>
2020-06-10 10:24:30 +0200

    Add audio functionality to the Mumble client

--- mumble/client.go
+++ mumble/client.go
@@ -3,6 +3,7 @@ package mumble
 import (
 	"strings"
 	"sync"
+	"time"
 
 	"layeh.com/gumble/gumble"
 	"layeh.com/gumble/gumbleutil"
@@ -19,7 +20,9 @@ const (
 type Client struct {
 	sync.Mutex
 	*gumble.Client
-	Out chan string
+	Out       chan string
+	audioOut  sync.Mutex
+	stopAudio bool
 }
 
 // NewClient initialises and returns a Mumble Client
@@ -60,4 +63,68 @@ func (c *Client) textMessageHandler(e *gumble.TextMessageEvent) {
 	if strings.HasPrefix(e.TextMessage.Message, "!") {
 		c.Out <- e.TextMessage.Message
 	}
+}
+
+// SendAudio sends the given 48 kHz 16-bit PCM audio to the main audio channel.
+// This function waits for any earlier SendAudio() or StreamAudio() calls to finish.
+func (c *Client) SendAudio(samples []int16) {
+	c.audioOut.Lock()
+	defer c.audioOut.Unlock()
+
+	out := c.AudioOutgoing()
+	defer close(out)
+
+	frameSize := c.Config.AudioFrameSize()
+	ticker := time.NewTicker(c.Config.AudioInterval)
+	for i := 0; i < len(samples)/frameSize; i++ {
+		out <- samples[i*frameSize : i*frameSize+frameSize]
+
+		if c.AudioStopped() {
+			break
+		}
+		<-ticker.C
+	}
+
+	c.stopAudio = false
+}
+
+// StreamAudio sends the given 48 kHz 16-bit PCM audio to the main audio channel.
+// This function waits for any earlier SendAudio() or StreamAudio() calls to finish.
+func (c *Client) StreamAudio(ch <-chan int16) {
+	c.audioOut.Lock()
+	defer c.audioOut.Unlock()
+
+	out := c.AudioOutgoing()
+	defer close(out)
+
+	ticker := time.NewTicker(c.Config.AudioInterval)
+	buf := make([]int16, 0, c.Config.AudioFrameSize())
+	for s := range ch {
+		buf = append(buf, s)
+		if len(buf) == cap(buf) {
+			out <- buf
+			buf = make([]int16, 0, c.Config.AudioFrameSize())
+			<-ticker.C
+		}
+	}
+
+	if len(buf) > 0 {
+		out <- buf
+	}
+
+	c.stopAudio = false
+}
+
+// AudioStopped returns true if audio should be stopped.
+func (c *Client) AudioStopped() bool {
+	c.Lock()
+	defer c.Unlock()
+	return c.stopAudio
+}
+
+// StopAudio requests all currently playing audio to be stopped.
+func (c *Client) StopAudio() {
+	c.Lock()
+	defer c.Unlock()
+	c.stopAudio = true
 }
