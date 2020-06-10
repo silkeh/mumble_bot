c3bf9e63887ddbb9685668dfd8c1d020b9de7130
Silke Hofstra <silke@slxh.eu>
2020-06-10 10:31:39 +0200

    Remove stickers in favour of config

--- matrix/sticker.go
+++ matrix/sticker.go
@@ -2,31 +2,16 @@ package matrix
 
 // Sticker represents a sticker message
 type Sticker struct {
-	URL       string      `json:"url"`
-	Body      string      `json:"body"`
-	RelatesTo error       `json:"m.relates_to"`
-	Info      StickerInfo `json:"info"`
+	URL       string      `json:"url" yaml:"url"`
+	Body      string      `json:"body" yaml:"body"`
+	RelatesTo error       `json:"m.relates_to" yaml:"relates_to"`
+	Info      StickerInfo `json:"info" yaml:"info"`
 }
 
 // StickerInfo contains additional sticker information
 type StickerInfo struct {
-	MimeType string `json:"mimetype"`
-	H        int    `json:"h"`
-	W        int    `json:"w"`
-	Size     int    `json:"size"`
-}
-
-// Stickers contains a preconfigured sticker set
-var Stickers = map[string]*Sticker{
-	"welcome": {
-		URL:       "mxc://integrations.snt.utwente.nl/JpHxNejcIeluxjzgGDNEwfWT",
-		Body:      "🍹 (tropical drink)",
-		RelatesTo: nil,
-		Info: StickerInfo{
-			MimeType: "image/webp",
-			H:        203,
-			W:        256,
-			Size:     101312,
-		},
-	},
+	MimeType string `json:"mimetype" yaml:"mime_type"`
+	H        int    `json:"h" yaml:"h"`
+	W        int    `json:"w" yaml:"w"`
+	Size     int    `json:"size" yaml:"size"`
 }
